eea7b7d1296e7e1c4b130213b1c906d4400e657d
zurazumba <zura.zumbadze@yahoo.com>
2023-06-18 00:06:24 +0400

    6-1

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,18 @@
 package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
-    public void roundNumber(float numberToBeRounded) {
+    public void roundNumber (float numberToBeRounded) {
+        int roundToFloor = (int) Math.floor(numberToBeRounded);
+        System.out.println(roundToFloor);
+        int roundToCeil = (int) Math.ceil(numberToBeRounded);
+        System.out.println(roundToCeil);
+    }
+
+
+    public static void main(String[] args) {
+        FloatTypeCasting floatTypeCasting = new FloatTypeCasting();
+        float number = 19.5F;
+        floatTypeCasting.roundNumber (number);
+
     }
 }
